fce10235b883afc9f04acac3c8d1e3ecf169a672
michaebourilwork <michaebourilwork@gmail.com>
2023-01-20 15:47:33 -0500

    Initial Commit

---
+++ BriskListPro/BriskListProApp.swift
@@ -0,0 +1,17 @@
+//
+//  BriskListProApp.swift
+//  BriskListPro
+//
+//  Created by Michael Bouril on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BriskListProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
